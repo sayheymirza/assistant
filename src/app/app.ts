ef88cea3b1aacf1e271dbcbefefbf6646ba7f427
Mirza <sayheymirza@gmail.com>
2025-11-22 19:40:40 +0330

    🔨 better ui experiance

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,18 +16,20 @@ import { Message } from "./message";
 
     <div class="flex-1"></div>
 
-    <app-wavesurfer />
-    
-    <div (click)="end()" class="flex flex-col items-center gap-4">
-      <button class="bg-red-500 w-14 h-14 rounded-full flex items-center justify-center">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4.51 15.48l2-1.59c.48-.38.76-.96.76-1.57v-2.6c3.02-.98 6.29-.99 9.32 0v2.61c0 .61.28 1.19.76 1.57l1.99 1.58c.8.63 1.94.57 2.66-.15l1.22-1.22c.8-.8.8-2.13-.05-2.88-6.41-5.66-16.07-5.66-22.48 0-.85.75-.85 2.08-.05 2.88l1.22 1.22c.71.72 1.85.78 2.65.15z"/></svg>
-      </button>
-
-      <strong class="opacity-70 text-sm">پایان</strong>
-    </div>
+    <footer class="fixed bottom-4 left-0 right-0 flex flex-col items-center justify-center gap-4 p-4">
+      <app-wavesurfer />
+      
+      <div (click)="end()" class="flex flex-col items-center gap-4">
+        <button class="bg-red-500 w-14 h-14 rounded-full flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4.51 15.48l2-1.59c.48-.38.76-.96.76-1.57v-2.6c3.02-.98 6.29-.99 9.32 0v2.61c0 .61.28 1.19.76 1.57l1.99 1.58c.8.63 1.94.57 2.66-.15l1.22-1.22c.8-.8.8-2.13-.05-2.88-6.41-5.66-16.07-5.66-22.48 0-.85.75-.85 2.08-.05 2.88l1.22 1.22c.71.72 1.85.78 2.65.15z"/></svg>
+        </button>
+        
+        <strong class="opacity-70 text-sm">پایان</strong>
+      </div>
+    </footer>
   `,
   host: {
-    class: 'relative w-screen h-screen flex flex-col items-center gap-4 p-10'
+    class: 'w-full h-full min-h-full flex flex-col items-center gap-4 p-10'
   }
 })
 export class App {
