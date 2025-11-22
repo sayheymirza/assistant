0bdf3d20446b52a5fa72e950e2f16c1be9944ca6
Mirza <sayheymirza@gmail.com>
2025-11-22 19:35:48 +0330

    🔨 better ui experiance

--- src/app/app.ts
+++ src/app/app.ts
@@ -27,7 +27,7 @@ import { Message } from "./message";
     </div>
   `,
   host: {
-    class: 'relative w-vw h-vh flex flex-col items-center gap-4 p-10'
+    class: 'relative w-screen h-screen flex flex-col items-center gap-4 p-10'
   }
 })
 export class App {
