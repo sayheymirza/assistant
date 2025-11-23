5465953fdf30241783a4e16bea55cd88f7ec67b6
Mirza <sayheymirza@gmail.com>
2025-11-23 12:07:50 +0330

    🔨 refactor app structure and add call component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,69 +1,30 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { Face } from './face';
-import { Wavesurfer } from "./wavesurfer";
-import { Assistant } from './assistant';
-import { Message } from "./message";
-import { Footer } from "./footer";
-import { Background } from "./background";
+import { Component, signal } from '@angular/core';
+import { Call } from "./call";
 
 @Component({
   selector: 'app-root',
-  imports: [Face, Wavesurfer, Message, Footer, Background],
+  imports: [Call],
   template: `
-    <app-background />
+    @if(isPWA() == true) {
+      <app-call />
+    }
 
-    <strong class="text-4xl">{{time()}}</strong>
-
-    <app-face class="block my-10" [style]="faceStyle()"/>
-
-    <app-message [item]="response()"/>
-
-    <div class="flex-1"></div>
-
-    <footer class="fixed bottom-4 left-0 right-0 flex flex-col items-center justify-center gap-4 p-4">
-      <app-wavesurfer />
-      
-      <app-footer 
-        (end)="end()"
-      />
-    </footer>
-  `,
-  host: {
-    class: 'w-full h-full min-h-full flex flex-col items-center gap-4 pt-20'
-  }
+    @if(isPWA() == false) {
+      <div class="flex flex-col items-center justify-center h-screen w-screen">
+        <strong class="text-8xl">PWA</strong>
+        <span class="opacity-70">Please add to home screen</span>
+      </div>
+    }
+  `
 })
 export class App {
-  private assistant = inject(Assistant);
+  public isPWA = signal<boolean | null>(null);
 
-  public time = signal('0:00');
-  public response = signal<any>(null);
-
-  public faceStyle = computed(() => {
-    return this.response() ? 'responsed' : 'normal';
-  });
-
-  ngOnInit() {
-    setInterval(() => {
-      const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      this.time.set(`${hours}:${minutes}`);
-    }, 1000);
-
-    // Simulate a response after 5 seconds
-    setTimeout(() => {
-      this.response.set({ type: 'message', content: 'سلام 👋\nچطور می تونم بهت کمک کنم؟\nمثلا می تونی بگی : من یک دونه اسپرسو دبل می خواهم' });
-    }, 5000);
-
-    setTimeout(() => {
-
-      this.assistant.start().then(() => {
-        console.log('Recording started');
-      });
-    }, 100);
-  }
-
-  public end() {
-    this.assistant.end();
+  ngAfterViewInit() {
+    if (window.matchMedia('(display-mode: standalone)').matches || (window.navigator as any).standalone === true || (window.location.hostname == 'localhost')) {
+      this.isPWA.set(true);
+    } else {
+      this.isPWA.set(false);
+    }
   }
 }
---
+++ src/app/call.ts
@@ -0,0 +1,69 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { Face } from './face';
+import { Wavesurfer } from "./wavesurfer";
+import { Assistant } from './assistant';
+import { Message } from "./message";
+import { Footer } from "./footer";
+import { Background } from "./background";
+
+@Component({
+  selector: 'app-call',
+  imports: [Face, Wavesurfer, Message, Footer, Background],
+  template: `
+    <app-background />
+
+    <strong class="text-3xl opacity-70">{{time()}}</strong>
+
+    <app-face class="block my-10" [style]="faceStyle()"/>
+
+    <app-message [item]="response()"/>
+
+    <div class="flex-1"></div>
+
+    <footer class="fixed bottom-4 left-0 right-0 flex flex-col items-center justify-center gap-4 p-4">
+      <app-wavesurfer />
+      
+      <app-footer 
+        (end)="end()"
+      />
+    </footer>
+  `,
+  host: {
+    class: 'w-full h-full min-h-full flex flex-col items-center gap-4 pt-14'
+  }
+})
+export class Call {
+  private assistant = inject(Assistant);
+
+  public time = signal('0:00');
+  public response = signal<any>(null);
+
+  public faceStyle = computed(() => {
+    return this.response() ? 'responsed' : 'normal';
+  });
+
+  ngOnInit() {
+    setInterval(() => {
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      this.time.set(`${hours}:${minutes}`);
+    }, 1000);
+
+    // Simulate a response after 5 seconds
+    setTimeout(() => {
+      this.response.set({ type: 'message', content: 'سلام 👋\nچطور می تونم بهت کمک کنم؟\nمثلا می تونی بگی : من یک دونه اسپرسو دبل می خواهم' });
+    }, 5000);
+
+    setTimeout(() => {
+
+      this.assistant.start().then(() => {
+        console.log('Recording started');
+      });
+    }, 100);
+  }
+
+  public end() {
+    this.assistant.end();
+  }
+}
